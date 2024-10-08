08df386c30f7cbd5d179d1ce4f55148c09bb801a
PranadityaTriJyotistaVavitramPS <gedearisudana16@gmail.com>
2024-10-09 01:10:56 +0700

    Darkmode kak

--- src/components/HeaderComp.js
+++ src/components/HeaderComp.js
@@ -8,6 +8,7 @@ function HeaderComp(){
     return( 
     <div className="header-comp">
         <SearchBarComp />
+        <ThemeToggleComp/>
     </div>
     );
 }
--- src/components/ThemeToggleComp.js
+++ src/components/ThemeToggleComp.js
@@ -1,10 +1,21 @@
 import './Components.css';
+import React,{useState} from 'react';
 
 function ThemeToggleComp(){
+
+    const[isLightMode,setIsLightMode] = useState(true);
+
+    const handleThemeToggle = () =>{
+        setIsLightMode(!isLightMode);
+        document.documentElement.classList.toggle('dark', isLightMode);
+    }
+
     return(
-        <div className="theme-toggle-comp">
-        <h1>HELLO WORLD</h1>
-</div>
+        <div className = {`theme-toggle-comp ${isLightMode ? 'light' : 'dark'}`}>
+            <button className="theme-toggle" onClick={handleThemeToggle}>
+                {isLightMode ? '☀️' : '🌙'}
+            </button>
+        </div>
     );
 }
 
