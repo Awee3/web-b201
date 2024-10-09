60b55aa38b892cb5ceba5c4523af23858a043786
Awee3 <syahputraahmad707@gmail.com>
2024-10-09 17:45:16 +0700

    tes

--- src/api/dockerApi.js
+++ src/api/dockerApi.js
@@ -1,13 +1,13 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_URL = 'http://localhost:3000/images'// API
+const API_URL = "http://10.3.142.81:9191/api/images"; // API
 
 export const getDockerFiles = async () => {
   try {
     const response = await axios.get(API_URL);
     return response.data;
   } catch (error) {
-    console.error('Error fetching Docker files:', error);
+    console.error("Error fetching Docker files:", error);
     throw error;
   }
 };
